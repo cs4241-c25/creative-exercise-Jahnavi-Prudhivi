c49d0aadaa53e0a806df5e35ac6f9649a208b991
Jahnavi Prudhvi <jahnaviprudhivi2004@gmail.com>
2025-02-23 11:19:55 -0500

    test finisheD

---
+++ server.js
@@ -0,0 +1,15 @@
+
+// server.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
